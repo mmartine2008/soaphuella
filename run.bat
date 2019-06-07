5e7ef556fcf4a5e2af935140210469e0c4fcf1e0
mmartine2008 <mariano.andres.martinez@gmail.com>
2019-06-07 11:07:48 -0300

    Ajuste

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 cd bin
-java wsserver.HuellaServiceStarter http localhost 1212 huella
-#java wsserver.ProdemanSOAPClient
+java wsserver.HuellaServiceStarter http 192.168.1.102 1212 huella mocking
+#java wsserver.HuellaServiceStarter http 192.168.1.102 1212 huella 
 cd ..
