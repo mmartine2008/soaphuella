d2d2a3a29328b0bbd9e849f7455b689a3baa82dc
mmartine2008 <mariano.andres.martinez@gmail.com>
2019-06-09 21:39:10 -0300

    Ajuste

--- trash/Servicio.java
+++
@@ -1,9 +0,0 @@
-import javax.xml.ws.Endpoint;
- 
-public class Servicio {
-  public static void main(String[] args) {
-    Endpoint.publish(
-      "http://localhost:8080/ServicioWeb/Hello",
-       new Hello());
-  }
-}
